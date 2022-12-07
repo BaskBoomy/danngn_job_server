cbb514ee86bc9baee2ee9af4caeedc0586f5a25e
jack <whdgurtpqms@gmail.com>
2022-12-07 17:05:33 +0900

    add deploy.yml & update appspec.yml and after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/danngn_job_server/deploy.log
 
-echo 'cd / home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/danngn_job_server/deploy.log
+echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/danngn_job_server/deploy.log
+cd /home/ec2-user/danngn_job_server >> /home/ec2-user/danngn_job_server/deploy.log
 
 echo 'npm install' >> /home/ec2-user/danngn_job_server/deploy.log 
 npm install >> /home/ec2-user/danngn_job_server/deploy.log
